6b2fb7d207d99a253d7ea109cd80af85a7024c60
Patrick <ruomungai@gmail.com>
2022-09-11 21:58:51 +0300

    Updates

---
+++ precache-manifest.3f71499bea42a09a4b700b4e5650c761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a817781c67dea7fffa950ad2d3c3ed52",
+    "url": "/famer-app/index.html"
+  },
+  {
+    "revision": "5ce6fba9887aea9f644c",
+    "url": "/famer-app/static/css/main.4a7ca07d.chunk.css"
+  },
+  {
+    "revision": "126c81e1ea51398e40e4",
+    "url": "/famer-app/static/js/2.322a9d2c.chunk.js"
+  },
+  {
+    "revision": "dba0c006430f86ab5dfd81528eb42c84",
+    "url": "/famer-app/static/js/2.322a9d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce6fba9887aea9f644c",
+    "url": "/famer-app/static/js/main.4c816174.chunk.js"
+  },
+  {
+    "revision": "38a17ee2a09d68566e6c",
+    "url": "/famer-app/static/js/runtime-main.f51039af.js"
+  }
+]);
